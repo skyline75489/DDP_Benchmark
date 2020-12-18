d7e1399da8afb2b9caffbc1b4802f59ea3e8bffc
Chester Liu <skyline75489@outlook.com>
2020-12-18 17:19:23 +0800

    Create run_data_loader.sh

---
+++ run_data_loader.sh
@@ -0,0 +1,4 @@
+python imagenet_main.py /datadrive/azureuser/medium --arch resnet50 --gpu 0 --batch-size 128 --seed 0 --epoch 1  --data-loader --workers 1
+python imagenet_main.py /datadrive/azureuser/medium --arch resnet50 --gpu 0 --batch-size 128 --seed 0 --epoch 1  --data-loader --workers 4
+python imagenet_main.py /datadrive/azureuser/medium --arch resnet50 --gpu 0 --batch-size 128 --seed 0 --epoch 1  --data-loader --workers 6
+python imagenet_main.py /datadrive/azureuser/medium --arch resnet50 --gpu 0 --batch-size 128 --seed 0 --epoch 1  --data-loader --workers 8
